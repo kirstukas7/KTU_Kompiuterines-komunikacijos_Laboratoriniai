8ab60e643c3be37c5a5eafebe6ea1cfa58890751
kirstukas7 <49204315+kirstukas7@users.noreply.github.com>
2026-04-04 22:32:39 +0300

    Initial commit

---
+++ terminal.cpp
@@ -0,0 +1,12 @@
+﻿#include "terminal.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	GUI::terminal form;
+	Application::Run(% form);
+}
